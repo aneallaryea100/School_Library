f3b07fadc29f6dac8ed450365aae4ae5ced30a6f
Aneal Laryea <aneallaryea100@gmail.com>
2022-10-06 17:00:55 +0000

    Add association

--- person.rb
+++ person.rb
@@ -2,14 +2,15 @@ require_relative './nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..100),
           @name = name,
-          @age = age
-    @parent_permission = parent_permission
+          @age = age,
+          @parent_permission = parent_permission,
+          @rentals = []
   end
 
   def can_use_services?
