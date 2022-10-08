bc2b8bd819d14110bccce15f9adfde67f22ec3d3
Aneal Laryea <aneallaryea100@gmail.com>
2022-10-08 11:37:29 +0000

    Update main file UI

--- main.rb
+++ main.rb
@@ -9,16 +9,15 @@ class Main
   def option_list
     puts 'Welcome to school library app!'
     while @output != 7
-      puts " Please choose an option by entering a number:
-        1. List all books.
-        2. List all people
-        3. Create a person
-        4. Create a book.
-        5. Create a rental
-        6. List all rentals for a given person id.
-        7. Exit
+      puts ' Please choose an option by entering a number:  '
+      puts '1. List all books.'
+      puts '2. List all people'
+      puts '3. Create a person'
+      puts '4. Create a book.'
+      puts '5. Create a rental'
+      puts '6. List all rentals for a given person id.'
+      puts '7. Exit'
 
-        "
       start_app
     end
   end
